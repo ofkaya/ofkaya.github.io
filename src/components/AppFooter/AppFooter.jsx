af4e90fc3d16be1fb0f9c30fc2b031cea790533d
Ofkaya <ofek.britstein@gmail.com>
2024-06-07 13:32:50 +0300

    added modified by brosh 85

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -13,7 +13,7 @@ function AppFooter() {
             </div>
             <div className="text">
                 <p>אֱמֹר מְעַט וַעֲשֵׂה הַרְבֵּה</p>
-                <p>פותח על ידי פלוגה ב’ ברוש 82</p>
+                <p>פותח על ידי פלוגה ב’ ברוש 82 ועודכן על ידי פלוגת נבו ברוש 85</p>
                 <a href='https://forms.gle/CKAe4pDf7nHTZ3Q26' dir="rtl" target="_blank" className="link"
         rel="noreferrer">מצאתם באג? רוצים לדווח על תקלה? לחצו כאן</a>
             </div>
